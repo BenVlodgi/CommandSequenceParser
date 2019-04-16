076175cce7cfd34f2c27d439d2d7793fe81f7cba
Benjamin Blodgett <benvlodgi@gmail.com>
2019-04-16 01:52:04 -0400

    Create Extensions class to have some extra functionality for dealing with chars and strings

---
+++ CommandSequenceParser/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+
+namespace CommandSequenceParser
+{
+    public static class Extensions
+    {
+        public static char[] ToFixedCharArray(this string str, int length)
+        {
+            return str.PadRight(length).ToCharArray();
+        }
+        
+        public static string CharArrayToString(this char[] charArray)
+        {
+            var str = new string(charArray);
+            int length = str.IndexOf('\0');
+            if (length > -1)
+                str = str.Substring(0, length);
+            return str;
+        }
+
+        public static string ReadASCIIString(this BinaryReader binReader, int length)
+        {
+            return ASCIIEncoding.ASCII.GetString(binReader.ReadBytes(length));
+        }
+    }
+}
